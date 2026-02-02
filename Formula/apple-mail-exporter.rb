d3334ce31eca487a251a9f9011596f1fb50f4cde
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-02 12:48:54 -0500

    Fix SHA256 after repo made public

--- Formula/apple-mail-exporter.rb
+++ Formula/apple-mail-exporter.rb
@@ -4,7 +4,7 @@ class AppleMailExporter < Formula
   desc "Search and export emails from Apple Mail's local database by keyword"
   homepage "https://github.com/Aayush9029/apple-mail-exporter"
   url "https://github.com/Aayush9029/apple-mail-exporter/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "46924327017c061bbd1e06697e18b2d7d6e249c4da5f7ab0f755412348151463"
   license "MIT"
 
   depends_on "python@3.12"
