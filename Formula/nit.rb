a83f4c0d8dd6cb462967ed24320344ba2721c12b
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-05 15:42:01 -0500

    Add nit formula v0.1.0
    Browse tweets from the terminal via Nitter instances.
    Universal binary (arm64 + x86_64).

---
+++ Formula/nit.rb
@@ -0,0 +1,16 @@
+class Nit < Formula
+  desc "Browse tweets from the terminal via Nitter instances"
+  homepage "https://github.com/Aayush9029/nit"
+  url "https://github.com/Aayush9029/nit/releases/download/v0.1.0/nit-0.1.0-universal-macos.tar.gz"
+  sha256 "6555b07aa4fa38d5848760d8df95f794050861580601f7504f20a0835eab8daf"
+  license "MIT"
+  depends_on :macos
+
+  def install
+    bin.install "nit-0.1.0/nit"
+  end
+
+  test do
+    assert_match "USAGE", shell_output("#{bin}/nit --help")
+  end
+end
