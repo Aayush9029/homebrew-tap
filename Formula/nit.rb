f29c469370b4af74ee28e4d2831f993c4b10684e
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-05 15:46:28 -0500

    Update nit formula sha256 for rebuilt binary with stats fix

--- Formula/nit.rb
+++ Formula/nit.rb
@@ -2,7 +2,7 @@ class Nit < Formula
   desc "Browse tweets from the terminal via Nitter instances"
   homepage "https://github.com/Aayush9029/nit"
   url "https://github.com/Aayush9029/nit/releases/download/v0.1.0/nit-0.1.0-universal-macos.tar.gz"
-  sha256 "6555b07aa4fa38d5848760d8df95f794050861580601f7504f20a0835eab8daf"
+  sha256 "5285bb5e758fbf75e173707c074241dbbf7b7286340ace7e52e896041a0c24fc"
   license "MIT"
   depends_on :macos
 
