bf8372fec40beee3c48ab82afcb98d6601c8d653
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-20 15:11:51 -0500

    Update peek sha256 for v0.3.0

--- Formula/peek.rb
+++ Formula/peek.rb
@@ -2,7 +2,7 @@ class Peek < Formula
   desc "Describe images from the terminal using vision LLMs"
   homepage "https://github.com/Aayush9029/peek"
   url "https://github.com/Aayush9029/peek/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "9581dda97a081cfdf9870535ac6993ad6369850fc72d9a623a17d391df18bd0e"
+  sha256 "f35dea3d62e488aea74443298bb0c9ecec0fd6e205e3df6eb7840de22e15ecba"
   license "MIT"
   depends_on :macos
 
