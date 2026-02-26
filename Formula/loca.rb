79c787a1c2805bebb78ae2b8c1a5a2726b182f06
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-26 04:07:28 -0500

    Update loca v0.1.0 artifact hash

--- Formula/loca.rb
+++ Formula/loca.rb
@@ -2,7 +2,7 @@ class Loca < Formula
   desc "Get your Mac's current location from the terminal"
   homepage "https://github.com/Aayush9029/loca"
   url "https://github.com/Aayush9029/loca/releases/download/v0.1.0/loca-0.1.0-universal-macos.tar.gz"
-  sha256 "d204fe496874799d06f35661c8f2e422138516d4c0935b2dec02868ff6a4daad"
+  sha256 "c0e68577df5bf116350e1b5ae462c87cbc9db54dc80d7390d599839ae5540b54"
   license "MIT"
   depends_on :macos
 
