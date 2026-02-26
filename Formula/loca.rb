b852ca6e22a10b238778ec19abe2597ccb6965ad
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-26 03:39:03 -0500

    Update loca to v0.1.1

--- Formula/loca.rb
+++ Formula/loca.rb
@@ -1,8 +1,8 @@
 class Loca < Formula
   desc "Get your Mac's current location from the terminal"
   homepage "https://github.com/Aayush9029/loca"
-  url "https://github.com/Aayush9029/loca/releases/download/v0.1.0/loca-0.1.0-universal-macos.tar.gz"
-  sha256 "fbb7fcb6f6145513663c06576257cd37ffd19e438fa69c528a34d1bf08c0b186"
+  url "https://github.com/Aayush9029/loca/releases/download/v0.1.1/loca-0.1.1-universal-macos.tar.gz"
+  sha256 "e34addccaf7c62ee5617225c8f6260be5b8e30398a0899c03303b18d15fe74c8"
   license "MIT"
   depends_on :macos
 
@@ -11,6 +11,6 @@ class Loca < Formula
   end
 
   test do
-    assert_match "loca", shell_output("#{bin}/loca --version")
+    assert_match version.to_s, shell_output("#{bin}/loca --version")
   end
 end
