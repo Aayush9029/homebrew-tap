d7ed9723d0386f646f5d01ebaeec987d747baa29
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-28 19:27:29 -0500

    Update spu to v0.2.1

--- Formula/spu.rb
+++ Formula/spu.rb
@@ -1,8 +1,8 @@
 class Spu < Formula
   desc "Check and update Swift package dependencies"
   homepage "https://github.com/Aayush9029/spu"
-  url "https://github.com/Aayush9029/spu/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "991be8ecf5a30283047d9180da07a0979772f91a4c721be730bdf3209983b732"
+  url "https://github.com/Aayush9029/spu/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "88c350128cc2cb6752ba9cfdaba5bad680b06fbd6f9fa4f0a39b052619f20e8e"
   license "MIT"
   depends_on :macos
 
