a281f11cc6d5d581a7c6f3ecc167f0e0cec30d02
Aayush9029 <aayushpokharel9029@gmail.com>
2026-03-04 08:07:19 -0500

    Update tmail sha256

--- Formula/tmail.rb
+++ Formula/tmail.rb
@@ -2,7 +2,7 @@ class Tmail < Formula
   desc "Disposable email in your terminal"
   homepage "https://github.com/Aayush9029/tmail"
   url "https://github.com/Aayush9029/tmail/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9d58ebef7ccbdcb927785169883e840bb818548259cc626a18098b140667e0d8"
+  sha256 "eaba2e486070585d2d655dbe7b3c68e2ee50d5cb21afe5af06be000b0d82d42c"
   license "MIT"
   depends_on "go" => :build
 
