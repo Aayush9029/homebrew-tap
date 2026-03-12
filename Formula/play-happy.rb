b9ee6ac7f79261eaaf7f0e2d63322aa4b60ceb08
Aayush9029 <aayushpokharel9029@gmail.com>
2026-03-12 02:00:32 -0400

    Update play-happy sha256

--- Formula/play-happy.rb
+++ Formula/play-happy.rb
@@ -2,7 +2,7 @@ class PlayHappy < Formula
   desc "Celebrate wins with random meme videos and text overlays"
   homepage "https://github.com/Aayush9029/play-happy"
   url "https://github.com/Aayush9029/play-happy/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "86d36e7c1f3c223c837ae290bc0bfe509ccaa263b5fb9a40d6e05d2ef833cc69"
+  sha256 "ad21e166c9df00c2056c7cd13b2511d2acd0a142b3257b9bdd990d6e70aee121"
   license "MIT"
   depends_on :macos
   depends_on "mpv"
