a7ace3dea8eab0fdcda99f35059570819a769df2
Aayush9029 <aayushpokharel9029@gmail.com>
2026-03-25 14:49:57 -0400

    Update sip to v0.2.0

--- Formula/sip.rb
+++ Formula/sip.rb
@@ -1,8 +1,8 @@
 class Sip < Formula
   desc "Summarize stdin using LLMs"
   homepage "https://github.com/Aayush9029/sip"
-  url "https://github.com/Aayush9029/sip/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "513f990273fe4be95972f21740dc02a5e2b5138146879f5a06d1db8a5f84bcfa"
+  url "https://github.com/Aayush9029/sip/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "b0fc3d58b168b0a88940caddeea77b30ec046c5a4aebe7c3d2a8628947626b52"
   license "MIT"
   depends_on :macos
 
