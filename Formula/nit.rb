0befb88203b322ac6f2dccebf337f76804f33d36
Aayush9029 <aayushpokharel9029@gmail.com>
2026-04-03 00:36:23 -0400

    Update nit to v2.0.0 (Go rewrite)

--- Formula/nit.rb
+++ Formula/nit.rb
@@ -1,13 +1,15 @@
 class Nit < Formula
   desc "Personal Twitter feed reader for the terminal"
   homepage "https://github.com/Aayush9029/nit"
-  url "https://github.com/Aayush9029/nit/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "aa892307de063914f5e281c393b05295dea1949a7735a9d183a3f94c9dd013bb"
+  url "https://github.com/Aayush9029/nit/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "1374918acf30784174e43c807680c741ad8f867c81774514d164f71aa483fce3"
   license "MIT"
+
+  depends_on "go" => :build
   depends_on :macos
 
   def install
-    bin.install "bin/nit"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "."
   end
 
   test do
