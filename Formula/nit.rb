1d4524a182b4defe6ae32c4ff867151e344da6cd
Aayush9029 <aayushpokharel9029@gmail.com>
2026-04-03 00:09:58 -0400

    Update nit to v1.2.0

--- Formula/nit.rb
+++ Formula/nit.rb
@@ -1,8 +1,8 @@
 class Nit < Formula
   desc "Personal Twitter feed reader for the terminal"
   homepage "https://github.com/Aayush9029/nit"
-  url "https://github.com/Aayush9029/nit/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "55024f576bcbb8a5f45e0db2e2c14ab5352bbb3bc27a73f0f2208d36805c5860"
+  url "https://github.com/Aayush9029/nit/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "aa892307de063914f5e281c393b05295dea1949a7735a9d183a3f94c9dd013bb"
   license "MIT"
   depends_on :macos
 
