22a182a98f0eb47d46f49b98ae37f6fd6f465b4f
Aayush9029 <aayushpokharel9029@gmail.com>
2026-04-02 23:35:43 -0400

    Update nit to v1.0.0 (bash CLI rewrite)

--- Formula/nit.rb
+++ Formula/nit.rb
@@ -1,16 +1,16 @@
 class Nit < Formula
-  desc "Browse tweets from the terminal via X syndication"
+  desc "Personal Twitter feed reader for the terminal"
   homepage "https://github.com/Aayush9029/nit"
-  url "https://github.com/Aayush9029/nit/releases/download/v0.2.0/nit-0.2.0-universal-macos.tar.gz"
-  sha256 "933d8db630bb006ee1c1d71d2388ac2241ab53a9161600d8c917cc761f5a556c"
+  url "https://github.com/Aayush9029/nit/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "595db18bf33f08788f1b4720859cfdb4cf73bbac4cecab2392ba0c2279f7cd95"
   license "MIT"
   depends_on :macos
 
   def install
-    bin.install "nit"
+    bin.install "bin/nit"
   end
 
   test do
-    assert_match "USAGE", shell_output("#{bin}/nit --help")
+    assert_match version.to_s, shell_output("#{bin}/nit --version")
   end
 end
