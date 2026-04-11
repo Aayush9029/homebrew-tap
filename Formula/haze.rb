17427209bd76231a88ac50ce12588f8012a899f3
Aayush9029 <aayushpokharel9029@gmail.com>
2026-04-11 17:41:24 -0400

    Bump haze to 0.2.1

--- Formula/haze.rb
+++ Formula/haze.rb
@@ -1,8 +1,8 @@
 class Haze < Formula
   desc "Progressive blur, grain, and gradient color overlay for video"
   homepage "https://github.com/Aayush9029/haze"
-  url "https://github.com/Aayush9029/haze/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "17ba566cde89d4e21f8f2251f5048e0a57cdad199e313fef9b6a55bb59dc974f"
+  url "https://github.com/Aayush9029/haze/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "fc5d4411964e2d1a35dede494a2c30d9c339b5137f533ec6694ad1ff5fa139f6"
   license "MIT"
   depends_on "ffmpeg"
   depends_on :macos
