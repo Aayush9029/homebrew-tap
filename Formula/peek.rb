737b961d01c64d7671ce26128bb85a5f07e3d4b2
Aayush9029 <aayushpokharel9029@gmail.com>
2026-04-12 11:08:12 -0400

    Update peek to v0.5.0 (image compression)

--- Formula/peek.rb
+++ Formula/peek.rb
@@ -1,8 +1,8 @@
 class Peek < Formula
   desc "Describe images and videos from the terminal using vision LLMs"
   homepage "https://github.com/Aayush9029/peek"
-  url "https://github.com/Aayush9029/peek/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "5915634b2690287900527061df0f0f5c5e9aa86f6ba70b6c94d7e37ba0f8e3c4"
+  url "https://github.com/Aayush9029/peek/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "edaea0514c557903e95ff0b3417ea111be81500cc40ad931a2c225908455a5ae"
   license "MIT"
   depends_on :macos
 
