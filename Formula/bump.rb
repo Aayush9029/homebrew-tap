c65e7c151a92db8081c24489f4228caec27b0e7d
Aayush9029 <aayushpokharel9029@gmail.com>
2026-04-18 13:52:24 -0400

    Bump bump to v0.2.0

--- Formula/bump.rb
+++ Formula/bump.rb
@@ -1,8 +1,8 @@
 class Bump < Formula
   desc "Bump Xcode project marketing version"
   homepage "https://github.com/Aayush9029/bump"
-  url "https://github.com/Aayush9029/bump/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8c411d6f4955c300c106303ba34a41316beb8cbb1d8834908ed2f2856ceca834"
+  url "https://github.com/Aayush9029/bump/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "09cb9b6332359b51ad4ca70de48449491b8d6990911935c92136d717cdf7e8e5"
   license "MIT"
   depends_on :macos
 
