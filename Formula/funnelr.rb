fbc5cbae794fc4e08bfab23ca95adf7c2e28c0f6
Aayush9029 <aayushpokharel9029@gmail.com>
2026-04-24 22:18:11 -0400

    Update funnelr to 0.1.8

--- Formula/funnelr.rb
+++ Formula/funnelr.rb
@@ -1,8 +1,8 @@
 class Funnelr < Formula
   desc "Expose local web servers with Tailscale Funnel"
   homepage "https://github.com/Aayush9029/funnelr"
-  url "https://github.com/Aayush9029/funnelr/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "e011f17ccb4847b7ca3963d49b89f00939917e2ba14ca954d11a30b3bd7e787d"
+  url "https://github.com/Aayush9029/funnelr/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "7db460faed7a69675e591e2a6d8e9fdcc6aa79acb676b23cd668fe5b325f555e"
   license "MIT"
 
   depends_on "go" => :build
