19712c380027ddf91c2afda358f734cf3859cc65
Aayush9029 <aayushpokharel9029@gmail.com>
2026-05-27 20:02:33 -0400

    Bump strata to v0.6.0

--- Formula/strata.rb
+++ Formula/strata.rb
@@ -1,8 +1,8 @@
 class Strata < Formula
   desc "Translate missing Xcode string catalog entries with app-aware context"
   homepage "https://github.com/Aayush9029/strata"
-  url "https://github.com/Aayush9029/strata/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "b4fe4b6d8fb3cb3145f7bdb27793522b89dadaf18f276eeefb8e93f3a06b2e88"
+  url "https://github.com/Aayush9029/strata/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "a29573515c87d32034e84f9229b5b62a7779ee5eb5d17cc14c0b7cebf3e495b1"
   license "MIT"
 
   depends_on "go" => :build
